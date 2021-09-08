e251b1fe898395095f619c8e05ea901f6152c1af
Kallil <kallil.belmonte@gmail.com>
2021-09-08 06:59:22 -0300

    Files updated.

--- files/BR/cities/AP.ts
+++ files/BR/cities/AP.ts
@@ -1,5 +1,86 @@
 import { Cities } from './_types';
 
-const AP: Cities = {};
+const AP: Cities = {
+  Amapá: {
+    name: 'Amapá',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Calçoene: {
+    name: 'Calçoene',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Cutias: {
+    name: 'Cutias',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  'Ferreira Gomes': {
+    name: 'Ferreira Gomes',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Itaubal: {
+    name: 'Itaubal',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  'Laranjal do Jari': {
+    name: 'Laranjal do Jari',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Macapá: {
+    name: 'Macapá',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Mazagão: {
+    name: 'Mazagão',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Oiapoque: {
+    name: 'Oiapoque',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  'Pedra Branca do Amapari': {
+    name: 'Pedra Branca do Amapari',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  'Porto Grande': {
+    name: 'Porto Grande',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Pracuúba: {
+    name: 'Pracuúba',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Santana: {
+    name: 'Santana',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  'Serra do Navio': {
+    name: 'Serra do Navio',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  Tartarugalzinho: {
+    name: 'Tartarugalzinho',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+  'Vitória do Jari': {
+    name: 'Vitória do Jari',
+    state: 'Amapá',
+    stateInitials: 'AP',
+  },
+};
 
 export default AP;
