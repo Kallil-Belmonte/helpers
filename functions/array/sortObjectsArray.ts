222658c5793f4d63213243cc36a165ea47533658
Kallil <kallil.belmonte@gmail.com>
2021-09-10 16:44:42 -0300

    File updated.

--- functions/array/sortObjectsArray.ts
+++ functions/array/sortObjectsArray.ts
@@ -7,8 +7,8 @@ const sortObjectsArray = (array: any[], property: string, reverse = false) => {
   const propertiesSorted = array.map(item => item[property]).sort();
   const properetiesResult = reverse ? propertiesSorted.reverse() : propertiesSorted;
 
-  return properetiesResult.map(item => {
-    const object = array.find(arrayItem => arrayItem[property] === item);
+  return properetiesResult.map(propertyResult => {
+    const object = array.find(item => item[property] === propertyResult);
     return object;
   });
 };
