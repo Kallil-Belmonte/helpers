e89dab4bfdce36745949d3fbdffe6134b17d1d75
Kallil <kallil.belmonte@gmail.com>
2022-08-30 07:38:41 -0300

    File updated.

--- functions/date/getDayShift.ts
+++ functions/date/getDayShift.ts
@@ -7,16 +7,16 @@
 
 const getDayShift = () => {
   const SHIFTS = {
-    morging: 'Morging',
+    morning: 'Morning',
     afternoon: 'Afternoon',
     night: 'Night',
   };
-  const { morging, afternoon, night } = SHIFTS;
+  const { morning, afternoon, night } = SHIFTS;
 
   const currentDate = new Intl.DateTimeFormat('pt-BR', { hour: 'numeric' }).format(new Date());
   const currentHour = Number(currentDate);
 
-  let shift = morging;
+  let shift = morning;
   if (currentHour >= 12 && currentHour < 19) shift = afternoon;
   else if (currentHour >= 19 || currentHour < 6) shift = night;
   return shift;
