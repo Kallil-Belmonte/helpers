a2ebeadbfb2ce9a48c76842da0dce81b4b01f6e8
Kallil Belmonte <kallil.belmonte@gmail.com>
2022-12-01 08:45:04 -0300

    File updated.

--- functions/time/getTimeDifference.ts
+++ functions/time/getTimeDifference.ts
@@ -6,15 +6,16 @@
  */
 
 const getTimeDifference = (dateFrom: string | Date, dateTo?: string | Date) => {
+  const { floor } = Math;
   const dateFromResult = typeof dateFrom === 'string' ? new Date(dateFrom) : dateFrom;
   const dateToResult = typeof dateTo === 'string' ? new Date(dateTo) : dateTo || new Date();
 
   const milliseconds = Number(dateToResult) - Number(dateFromResult);
-  const seconds = milliseconds / 1000;
-  const minutes = seconds / 60;
-  const hours = minutes / 60;
-  const days = hours / 24;
-  const weeks = days / 7;
+  const seconds = floor(milliseconds / 1000);
+  const minutes = floor(seconds / 60);
+  const hours = floor(minutes / 60);
+  const days = floor(hours / 24);
+  const weeks = floor(days / 7);
 
   return {
     milliseconds,
