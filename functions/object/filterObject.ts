9c8c3bcbe8ca07724ac21c93c7f9af503f908549
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-09-23 22:20:31 -0300

    File updated.

--- functions/object/filterObject.ts
+++ functions/object/filterObject.ts
@@ -3,20 +3,24 @@ type ObjectType = { [key: string]: any };
 /**
  * @function filterObject
  * @description Returns a new object with or without the properties provided in the second parameter.
- * @param { Object } object - Object to be filtered.
- * @param { Object } propertiesToFilter - Properties to filter.
+ * @param { ObjectType } object - Object to be filtered.
+ * @param { string[] } propertiesToFilter - Properties to filter.
  * @param { boolean } [removeProperties=true] - If true, removes the properties to filter.
  * @author Kallil Belmonte
  * @see CodePen {@link https://codepen.io/kallil-belmonte/full/xxxpKxv}
  */
 
-const filterObject = (object: ObjectType, propertiesToFilter: string[], removeProperties = true) =>
+const filterObject = <Type = ObjectType>(
+  object: ObjectType,
+  propertiesToFilter: string[],
+  removeProperties = true,
+) =>
   Object.keys(object).reduce((accumulator: ObjectType, value) => {
     if (propertiesToFilter.includes(value) !== removeProperties) {
       accumulator[value] = object[value];
     }
 
     return accumulator;
-  }, {});
+  }, {}) as Type;
 
 export default filterObject;
