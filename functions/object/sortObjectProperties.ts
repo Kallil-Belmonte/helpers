0bc0919729b6992ed540b03891d610ec68714054
Kallil Belmonte <kallil.belmonte@gmail.com>
2024-09-07 09:40:23 -0300

    Helper updated.

--- functions/object/sortObjectProperties.ts
+++ functions/object/sortObjectProperties.ts
@@ -12,10 +12,10 @@ type ObjectType = { [key: string]: any };
 const sortObjectProperties = (object: ObjectType, reverse?: boolean): ObjectType => {
   const { keys } = Object;
   const format = (value: string) => value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  const propertiesSorted = keys(object)
+  const propertiesResult = keys(object)
     .map(key => format(key))
     .sort();
-  const propertiesResult = reverse ? propertiesSorted.reverse() : propertiesSorted;
+  if (reverse) propertiesResult.reverse();
 
   return propertiesResult.reduce((accumulator, value) => {
     const index = keys(object).findIndex(key => format(key) === format(value));
