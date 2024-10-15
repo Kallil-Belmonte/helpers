c047da332c67a5bb74476ed330f4eab6bcb3e56f
Kallil Belmonte <kallil.belmonte@gmail.com>
2024-10-15 08:06:30 -0300

    Helpers updated.

--- functions/data/uuid.ts
+++ functions/data/uuid.ts
@@ -5,12 +5,6 @@
  * @see CodePen {@link https://codepen.io/kallil-belmonte/full/abYgKPZ}
  */
 
-const uuid = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
+const uuid = () => window.crypto.randomUUID();
 
 export default uuid;
