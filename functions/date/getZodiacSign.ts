a963bb7d554f9ea40602195143ac7d5eb4ce32ec
Kallil Belmonte <kallil.belmonte@proton.me>
2026-01-23 19:44:06 -0300

    Helper updated.

--- functions/date/getZodiacSign.ts
+++ functions/date/getZodiacSign.ts
@@ -13,25 +13,21 @@ type Params = {
  */
 
 const getZodiacSign = (params: Params) => {
-  const { date, day: dayParam, month: monthParam } = params;
-  const day = date ? date.getDay() : dayParam || 1;
-  const month = date ? date.getMonth() + 1 : monthParam || 1;
+  const day = params.date ? params.date.getDate() : params.day || 1;
+  const month = params.date ? params.date.getMonth() + 1 : params.month || 1;
 
-  const from = (fromDay: number, fromMonth: number) => day >= fromDay && month === fromMonth;
-  const to = (toDay: number, toMonth: number) => day <= toDay && month === toMonth;
-
-  if (from(20, 1) || to(18, 2)) return 'Aquarius';
-  if (from(19, 2) || to(20, 3)) return 'Pisces';
-  if (from(21, 3) || to(19, 4)) return 'Aries';
-  if (from(20, 4) || to(20, 5)) return 'Taurus';
-  if (from(21, 5) || to(20, 6)) return 'Gemini';
-  if (from(21, 6) || to(22, 7)) return 'Cancer';
-  if (from(23, 7) || to(22, 8)) return 'Leo';
-  if (from(23, 8) || to(22, 9)) return 'Virgo';
-  if (from(23, 9) || to(22, 10)) return 'Libra';
-  if (from(23, 10) || to(21, 11)) return 'Scorpio';
-  if (from(22, 11) || to(21, 12)) return 'Sagittarius';
-  if (from(22, 12) || to(19, 1)) return 'Capricorn';
+  if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) return 'Aquarius';
+  if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) return 'Pisces';
+  if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) return 'Aries';
+  if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) return 'Taurus';
+  if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) return 'Gemini';
+  if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) return 'Cancer';
+  if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) return 'Leo';
+  if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) return 'Virgo';
+  if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) return 'Libra';
+  if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) return 'Scorpio';
+  if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) return 'Sagittarius';
+  if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) return 'Capricorn';
 
   return '';
 };
